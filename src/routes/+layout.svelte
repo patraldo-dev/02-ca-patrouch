c291258a6bf000e77fb9472f71971da0ebac51ed
Chef Tech <cheftech@patraldo.com>
2025-09-15 14:59:10 -0600

    add favicon.ico

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,6 +15,8 @@
 
 <svelte:head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" href="/favicon.ico" />
+
 </svelte:head>
 
 <div class="app-layout">
