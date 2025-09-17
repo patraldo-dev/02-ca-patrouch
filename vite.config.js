83564026b86049d22f78592c3299349e189d989a
Chef Tech <cheftech@patraldo.com>
2025-09-16 23:40:41 -0600

    fix oslo password error by adding to wrangler

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
 	define: {
         'process.env.OSLO_PASSWORD_DISABLE_NATIVE': '"1"'
     }
