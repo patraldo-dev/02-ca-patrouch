0baf1b3acc3fdb5888b75b73c2953f04e7da12ac
Chef Tech <cheftech@patraldo.com>
2026-04-06 21:54:55 -0600

    remove X/Twitter share button from card page

--- src/routes/card/[id]/+page.svelte
+++ src/routes/card/[id]/+page.svelte
@@ -35,11 +35,6 @@
         window.open(`https://t.me/share/url?url=${encodeURIComponent(shareUrl)}&text=${encodeURIComponent(shareText)}`, '_blank');
     }
 
-    function shareTwitter() {
-        const truncated = shareText.length > 200 ? shareText.slice(0, 200) + '…' : shareText;
-        window.open(`https://twitter.com/intent/tweet?text=${encodeURIComponent(truncated)}&url=${encodeURIComponent(shareUrl)}`, '_blank');
-    }
-
     function shareNative() {
         if (navigator.share) {
             navigator.share({ title, text: shareText, url: shareUrl });
@@ -102,9 +97,6 @@
             </button>
             <button class="share-btn telegram" onclick={shareTelegram} title="Share on Telegram">
                 <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor"><path d="M11.944 0A12 12 0 0 0 0 12a12 12 0 0 0 12 12 12 12 0 0 0 12-12A12 12 0 0 0 12 0a12 12 0 0 0-.056 0zm4.962 7.224c.1-.002.321.023.465.14a.506.506 0 0 1 .171.325c.016.093.036.306.02.472-.18 1.898-.962 6.502-1.36 8.627-.168.9-.499 1.201-.82 1.23-.696.065-1.225-.46-1.9-.902-1.056-.693-1.653-1.124-2.678-1.8-1.185-.78-.417-1.21.258-1.91.177-.184 3.247-2.977 3.307-3.23.007-.032.014-.15-.056-.212s-.174-.041-.249-.024c-.106.024-1.793 1.14-5.061 3.345-.479.33-.913.49-1.302.48-.428-.008-1.252-.241-1.865-.44-.752-.245-1.349-.374-1.297-.789.027-.216.325-.437.893-.663 3.498-1.524 5.83-2.529 6.998-3.014 3.332-1.386 4.025-1.627 4.476-1.635z"/></svg>
-            </button>
-            <button class="share-btn twitter" onclick={shareTwitter} title="Share on X / Twitter">
-                <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/></svg>
             </button>
             {#if typeof navigator !== 'undefined' && navigator.share}
                 <button class="share-btn native" onclick={shareNative} title="Share…">
@@ -303,12 +295,6 @@
         background: #0088cc;
         border-color: #0088cc;
         color: #fff;
-    }
-
-    .share-btn.twitter:hover {
-        background: #fff;
-        border-color: #fff;
-        color: #000;
     }
 
     .share-btn.native:hover {
