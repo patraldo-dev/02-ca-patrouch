e42620b9466b44b9683929eb3e4cc08d9177a0d0
Chef Tech <cheftech@patraldo.com>
2026-04-09 16:55:05 -0600

    Remove debug log

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -129,7 +129,6 @@
             onScroll();
             setTimeout(initObserver, 100);
             // Check onboarding for new users
-            console.log('onboarding check:', data?.user?.username, data?.onboarding_completed);
             if (data?.user && !data.onboarding_completed && !onboardingDismissed) {
                 showOnboarding = true;
             }
