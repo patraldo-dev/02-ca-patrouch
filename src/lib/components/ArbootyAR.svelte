3c779a8a6e55c66a0a4a739cbebea0edc9f49c73
Chef Tech <cheftech@patraldo.com>
2026-05-01 20:41:36 -0600

    feat: fanfare on activate button press

--- src/lib/components/ArbootyAR.svelte
+++ src/lib/components/ArbootyAR.svelte
@@ -259,6 +259,7 @@
 
     async function activate() {
         error = null;
+        playCapture();
         if (!(await requestOrientationPermission())) return;
         await startCamera();
         if (!cameraActive) return;
